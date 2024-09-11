d5cbace4bcb6c410f9b025e3ba8f89d6e5e63110
amystamile-usgs <astamile@usgs.gov>
2024-09-11 12:08:46 -0700

    Updated qisis files

--- isis/src/core/include/PvlObject.h
+++ isis/src/core/include/PvlObject.h
@@ -11,6 +11,8 @@ find files of those names at the top level of this repository. **/
 #include "PvlContainer.h"
 #include "PvlGroup.h"
 
+#include <QMetaType>
+
 #include <nlohmann/json.hpp>
 
 
@@ -354,5 +356,7 @@ namespace Isis {
                                                 in the current PvlObject. */
   };
 }
+
+Q_DECLARE_METATYPE(Isis::PvlObject);
 
 #endif
